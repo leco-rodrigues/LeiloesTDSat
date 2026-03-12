bae0780fb871c3a902572f3e0a9f349de896319d
Alex Rodrigues <alex2199rodrigues@gmail.com>
2026-03-12 16:06:58 -0300

    Método para vender produto adicionado

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -77,8 +77,26 @@ public class ProdutosDAO {
         return listagem;
 }
     
-    
-    
-        
+    public void venderProduto(int id) {
+
+        conn = new conectaDAO().connectDB();
+
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+
+        try {
+
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+
+            prep.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+
+        } catch(Exception e) {
+
+            JOptionPane.showMessageDialog(null, "Erro ao vender produto!");
+
+        }
+    }
 }
 
